457c61d0e7b3fccdf7034648d06a9b2f16f411bc
Ariel Oak <codestothestars@yahoo.com>
2020-01-11 14:14:01 -0500

    Make more beasts tameable
    Wooly Kodo and Shadowhorn Stag to start

---
+++ sql/migrations/c0005_tame_more_beasts.sql
@@ -0,0 +1,4 @@
+-- Make additional beasts tameable
+
+UPDATE `creature_template` SET `type_flags` = 1, `beast_family` =  5 WHERE `entry` = 3237; -- Wooly Kodo
+UPDATE `creature_template` SET `type_flags` = 1, `beast_family` = 24 WHERE `entry` = 3817; -- Shadowhorn Stag
