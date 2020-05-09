84bce5be478169ecc4c03cf030aedf392eafbeed
Ariel Oak <codestothestars@yahoo.com>
2020-05-09 14:03:28 -0400

    Increase pet experience multiplier from 2 to 3
    2 has felt not quite high enough.

--- src/game/Objects/Pet.cpp
+++ src/game/Objects/Pet.cpp
@@ -1206,7 +1206,7 @@ void Pet::DelayedUnsummon(uint32 timeMSToDespawn, PetSaveMode mode)
 
 void Pet::GivePetXP(uint32 xp)
 {
-    xp *= 2;
+    xp *= 3;
     
     if (getPetType() != HUNTER_PET)
         return;
