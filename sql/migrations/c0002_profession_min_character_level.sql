98221bc3ed1244f27e2ce583343f03245944b1df
Ariel Oak <codestothestars@yahoo.com>
2020-05-21 17:49:46 -0400

    Comment profession min char level migration
    Readability.

--- sql/migrations/c0002_profession_min_character_level.sql
+++ sql/migrations/c0002_profession_min_character_level.sql
@@ -3,4 +3,11 @@
 
 UPDATE npc_trainer
 SET reqlevel = 0
-WHERE spell IN (2021, 3539, 4041, 10663, 10769, 11612);
+WHERE spell IN (
+     2021, -- Journeyman Blacksmith
+     3539, -- Expert Blacksmith
+     4041, -- Expert Engineer
+    10663, -- Artisan Leatherworking
+    10769, -- Artisan Skinning
+    11612  -- Artisan Alchemist
+);
