7dd9d8a1067f16727c28d1f41453b575c37bb65f
Ariel Oak <codestothestars@yahoo.com>
2020-05-21 21:40:07 -0400

    Remove min char level from One-Handed Swords
    Involves relabeling the previous "profession" minimum character level
    migration to be applicable to any skill, rather than just professions.

--- sql/migrations/c0002_skill_min_character_level.sql
+++ sql/migrations/c0002_skill_min_character_level.sql
@@ -1,9 +1,10 @@
--- Remove minimum character level for professions.
+-- Remove minimum character level for skills.
 -- For our purposes, this limit on the player is unnecessary.
 
 UPDATE npc_trainer
 SET reqlevel = 0
 WHERE spell IN (
+     1847, -- One-Handed Swords
      2021, -- Journeyman Blacksmith
      3539, -- Expert Blacksmith
      4041, -- Expert Engineer
