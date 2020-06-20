3f745a67b4360c4c5d5127c434b54a8c59ee0201
Ariel Oak <codestothestars@yahoo.com>
2020-06-19 21:41:56 -0400

    Reduce grinding for some quests and rep

---
+++ sql/migrations/c0009_reduced_grinding.sql
@@ -0,0 +1,10 @@
+UPDATE `creature_loot_template` SET `maxcount` = 2, `mincountOrRef` = 2 WHERE `item` = 8153; -- Wildvine
+
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  80 WHERE `creature_id` IN (7153, 7154, 7155);   -- Deadwood Warrior, Gardener, Pathfinder
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 400 WHERE `creature_id` =  9464 AND `patch` = 0; -- Overlord Ror (Patch <1.9)
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 240 WHERE `creature_id` =  9464 AND `patch` = 7; -- Overlord Ror (Patch 1.9+)
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  80 WHERE `creature_id` = 14342 AND `patch` = 0; -- Ragepaw (Patch <1.9)
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 400 WHERE `creature_id` = 14342 AND `patch` = 7; -- Ragepaw (Patch 1.9+)
+
+UPDATE `quest_template` SET `RewRepValue1` =  100 WHERE `entry` = 6031; -- Runecloth
+UPDATE `quest_template` SET `RewRepValue1` = 1600 WHERE `entry` = 6131; -- Timbermaw Ally
