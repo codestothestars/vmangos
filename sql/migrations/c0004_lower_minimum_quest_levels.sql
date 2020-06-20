360014665fbd12a863dcf9175504734c4d8ee8e9
Ariel Oak <codestothestars@yahoo.com>
2020-06-19 21:41:22 -0400

    Remove min quest level for Timbermaw Hold
    No real reason to restrict these by level.

--- sql/migrations/c0004_lower_minimum_quest_levels.sql
+++ sql/migrations/c0004_lower_minimum_quest_levels.sql
@@ -1,4 +1,5 @@
 -- Lower minimum levels of some quests
 -- for more adventurous questing.
 
+UPDATE `quest_template` SET `MinLevel` =  0 WHERE `entry` IN (6031, 6032);                               -- Timbermaw Hold
 UPDATE `quest_template` SET `MinLevel` = 40 WHERE `entry` IN (7028, 7029, 7041, 7044, 7046, 7064, 7065); -- Maraudon
