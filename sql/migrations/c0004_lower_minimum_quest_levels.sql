bb001f82b6e37a55e5d4f2de0280143fb6e2c7b0
Ariel Oak <codestothestars@yahoo.com>
2020-09-01 18:06:23 -0400

    Remove min level from Blacksmith quests
    For our purposes, this limit on the player is unnecessary.

--- sql/migrations/c0004_lower_minimum_quest_levels.sql
+++ sql/migrations/c0004_lower_minimum_quest_levels.sql
@@ -1,5 +1,6 @@
 -- Lower minimum levels of some quests
--- for more adventurous questing.
+-- for role-playing and more adventurous questing.
 
 UPDATE `quest_template` SET `MinLevel` =  0 WHERE `entry` IN (6031, 6032);                               -- Timbermaw Hold
+UPDATE `quest_template` SET `MinLevel` =  0 WHERE `RequiredSkill` = 164;                                 -- Blacksmithing
 UPDATE `quest_template` SET `MinLevel` = 40 WHERE `entry` IN (7028, 7029, 7041, 7044, 7046, 7064, 7065); -- Maraudon
