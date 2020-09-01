9284481b49d12149ceed19c4f3e51b218f00b223
Ariel Oak <codestothestars@yahoo.com>
2020-09-01 18:13:48 -0400

    Allow having both Armor- and Weaponsmithing
    For fun.

--- sql/migrations/c0008_multiple_specializations.sql
+++ sql/migrations/c0008_multiple_specializations.sql
@@ -3,5 +3,6 @@
 UPDATE `quest_template`
 SET `ExclusiveGroup` = 0
 WHERE `entry` IN (
-    5141, 5143, 5144, 5145, 5146, 5148 -- Leatherworking
+    5141, 5143, 5144, 5145, 5146, 5148, -- Leatherworking
+    5283, 5284, 5301, 5302              -- Blacksmithing
 );
