76f0932fe1d680c023d1cb7b2a6f11acf770d579
Ariel Oak <codestothestars@yahoo.com>
2020-09-01 22:06:30 -0400

    Update cross-faction portals for latest vmangos
    IDs and table structure were still retained from an old deployment.

--- sql/migrations/c0006_cross_faction_portals.sql
+++ sql/migrations/c0006_cross_faction_portals.sql
@@ -5,32 +5,32 @@
 INSERT INTO `conditions`
 (`condition_entry`, `type`, `value1`, `value2`)
 VALUES
-(              141,     17,     3565,        1, 'Does not know Teleport: Darnassus'),
-(              142,     17,    11419,        1, 'Does not know Portal: Darnassus'),
-(              143,     17,     3562,        1, 'Does not know Teleport: Ironforge'),
-(              144,     17,    11416,        1, 'Does not know Portal: Ironforge'),
-(              145,     17,     3567,        1, 'Does not know Teleport: Orgrimmar'),
-(              146,     17,    11417,        1, 'Does not know Portal: Orgrimmar'),
-(              147,     17,     3561,        1, 'Does not know Teleport: Stormwind'),
-(              148,     17,    10059,        1, 'Does not know Portal: Stormwind'),
-(              149,     17,     3566,        1, 'Does not know Teleport: Thunder Bluff'),
-(              150,     17,    11420,        1, 'Does not know Portal: Thunder Bluff'),
-(              151,     17,     3563,        1, 'Does not know Teleport: Undercity'),
-(              152,     17,    11418,        1, 'Does not know Portal: Undercity'),
-(              179,     -1,       90,      178, 'Mage AND Level >= 40'),
-(             1020,     -1,       90,     1011, 'Mage AND Level >= 20'),
-(             1021,     -1,      141,     1020, 'Does not know Teleport: Darnassus AND Mage AND Level >= 20'),
-(             1022,     -1,      142,      179, 'Does not know Portal: Darnassus AND Mage AND Level >= 40'),
-(             1023,     -1,      143,     1020, 'Does not know Teleport: Ironforge AND Mage AND Level >= 20'),
-(             1024,     -1,      144,      179, 'Does not know Portal: Ironforge AND Mage AND Level >= 40'),
-(             1025,     -1,      145,     1020, 'Does not know Teleport: Orgrimmar AND Mage AND Level >= 20'),
-(             1026,     -1,      146,      179, 'Does not know Portal: Orgrimmar AND Mage AND Level >= 40'),
-(             1027,     -1,      147,     1020, 'Does not know Teleport: Stormwind AND Mage AND Level >= 20'),
-(             1028,     -1,      148,      179, 'Does not know Portal: Stormwind AND Mage AND Level >= 40'),
-(             1029,     -1,      149,     1020, 'Does not know Teleport: Thunder Bluff AND Mage AND Level >= 20'),
-(             1030,     -1,      150,      179, 'Does not know Portal: Thunder Bluff AND Mage AND Level >= 40'),
-(             1031,     -1,      151,     1020, 'Does not know Teleport: Undercity AND Mage AND Level >= 20'),
-(             1032,     -1,      152,      179, 'Does not know Portal: Undercity AND Mage AND Level >= 40');
+(              187,     -1,       90,      178), -- Mage AND Level >= 40
+(              216,     17,     3561,        1), -- Does not know Teleport: Stormwind
+(              217,     17,     3562,        1), -- Does not know Teleport: Ironforge
+(              218,     17,     3563,        1), -- Does not know Teleport: Undercity
+(              219,     17,     3565,        1), -- Does not know Teleport: Darnassus
+(              220,     17,     3566,        1), -- Does not know Teleport: Thunder Bluff
+(              221,     17,     3567,        1), -- Does not know Teleport: Orgrimmar
+(              222,     17,    10059,        1), -- Does not know Portal: Stormwind
+(              223,     17,    11416,        1), -- Does not know Portal: Ironforge
+(              224,     17,    11417,        1), -- Does not know Portal: Orgrimmar
+(              225,     17,    11418,        1), -- Does not know Portal: Undercity
+(              226,     17,    11419,        1), -- Does not know Portal: Darnassus
+(              227,     17,    11420,        1), -- Does not know Portal: Thunder Bluff
+(             1025,     -1,       90,     1011), -- Mage AND Level >= 20
+(             1026,     -1,      187,      222), -- Mage AND Level >= 40 AND Does not know Portal: Stormwind
+(             1027,     -1,      187,      223), -- Mage AND Level >= 40 AND Does not know Portal: Ironforge
+(             1028,     -1,      187,      224), -- Mage AND Level >= 40 AND Does not know Portal: Orgrimmar
+(             1029,     -1,      187,      225), -- Mage AND Level >= 40 AND Does not know Portal: Undercity
+(             1030,     -1,      187,      226), -- Mage AND Level >= 40 AND Does not know Portal: Darnassus
+(             1031,     -1,      187,      227), -- Mage AND Level >= 40 AND Does not know Portal: Thunder Bluff
+(             1032,     -1,      216,     1025), -- Does not know Teleport: Stormwind AND Mage AND Level >= 20
+(             1033,     -1,      217,     1025), -- Does not know Teleport: Ironforge AND Mage AND Level >= 20
+(             1034,     -1,      218,     1025), -- Does not know Teleport: Undercity AND Mage AND Level >= 20
+(             1035,     -1,      219,     1025), -- Does not know Teleport: Darnassus AND Mage AND Level >= 20
+(             1036,     -1,      220,     1025), -- Does not know Teleport: Thunder Bluff AND Mage AND Level >= 20
+(             1037,     -1,      221,     1025); -- Does not know Teleport: Orgrimmar AND Mage AND Level >= 20
 
 -- Gossip scripts to cast spell teaching the teleport or portal spell
 INSERT INTO `gossip_scripts`
@@ -61,15 +61,15 @@ DELETE FROM `gossip_menu_option` WHERE `menu_id` IN (
 -- Reuse existing default "teach me" portal trainer text for first custom option.
 UPDATE `broadcast_text`
 SET
-    `FemaleText` = 'Please teach me how to teleport to this city.',
-    `MaleText`   = 'Please teach me how to teleport to this city.'
-WHERE ID = 8442;
+    `female_text` = 'Please teach me how to teleport to this city.',
+    `male_text`   = 'Please teach me how to teleport to this city.'
+WHERE `entry` = 8442;
 
 -- Add new custom "teach me portal" text for second custom option.
 INSERT INTO `broadcast_text`
-(`ID`, `MaleText`,                                            `FemaleText`,                                          `Sound`, `Type`, `Language`, `EmoteId0`, `EmoteId1`, `EmoteId2`, `EmoteDelay0`, `EmoteDelay1`, `EmoteDelay2`)
+(`entry`, `male_text`,                                           `female_text`,                                         `sound_id`, `chat_type`, `language_id`, `emote_id0`, `emote_id1`, `emote_id2`, `emote_delay0`, `emote_delay1`, `emote_delay2`)
 VALUES
-(   2, 'Please teach me how to create portals to this city.', 'Please teach me how to create portals to this city.',       0,      0,          0,          0,          0,          0,             0,             0,             0);
+(      2, 'Please teach me how to create portals to this city.', 'Please teach me how to create portals to this city.',          0,           0,             0,           0,           0,           0,              0,              0,              0);
 
 -- Create new menus for Larimaine Purdue and Lexington Mortaim
 -- so each portal trainer can have their own gossip scripts.
@@ -95,33 +95,33 @@ VALUES
 
 -- Give Larimaine Purdue and Lexington Mortaim their new menus
 UPDATE `creature_template` SET `gossip_menu_id` = 1 WHERE `entry` = 2485; -- Larimaine Purdue
-UPDATE `creature_template` SET `gossip_menu_id` = 2 WHERE `entry` = 2485; -- Lexington Mortaim
+UPDATE `creature_template` SET `gossip_menu_id` = 2 WHERE `entry` = 2492; -- Lexington Mortaim
 
 -- Create new menu options, unique for each trainer, with custom options added.
 INSERT INTO `gossip_menu_option`
-(`menu_id`, `id`, `option_icon`, `option_text`,                                         `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `BoxBroadcastTextID`, `condition_id`)
+(`menu_id`, `id`, `option_icon`, `option_text`,                                         `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `box_broadcast_text`, `condition_id`)
 VALUES
-(        1,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 28,           0,           0, '',                            0,           1027), -- Larimaine Purdue
-(        1,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 29,           0,           0, '',                            0,           1028), -- Larimaine Purdue
+(        1,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 28,           0,           0, '',                            0,           1032), -- Larimaine Purdue
+(        1,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 29,           0,           0, '',                            0,           1026), -- Larimaine Purdue
 (        1,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                410,           0,           0, '',                            0,            462), -- Larimaine Purdue
 (        1,    4,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                411,           0,           0, '',                            0,            461), -- Larimaine Purdue
-(        2,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 32,           0,           0, '',                            0,           1031), -- Give this to Lexington Mortaim
-(        2,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 33,           0,           0, '',                            0,           1032), -- Give this to Lexington Mortaim
-(        2,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                418,           0,           0, '',                            0,            462), -- Give this to Lexington Mortaim
-(        2,    4,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                419,           0,           0, '',                            0,            461), -- Give this to Lexington Mortaim
-(     4822,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 22,           0,           0, '',                            0,           1021), -- Elissa Dumas
-(     4822,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 23,           0,           0, '',                            0,           1022), -- Elissa Dumas
+(        2,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 32,           0,           0, '',                            0,           1034), -- Lexington Mortaim
+(        2,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 33,           0,           0, '',                            0,           1029), -- Lexington Mortaim
+(        2,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                418,           0,           0, '',                            0,            462), -- Lexington Mortaim
+(        2,    4,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                419,           0,           0, '',                            0,            461), -- Lexington Mortaim
+(     4822,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 22,           0,           0, '',                            0,           1035), -- Elissa Dumas
+(     4822,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 23,           0,           0, '',                            0,           1030), -- Elissa Dumas
 (     4822,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                410,           0,           0, '',                            0,            462), -- Elissa Dumas
 (     4822,    4,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                411,           0,           0, '',                            0,            461), -- Elissa Dumas
-(     4823,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 24,           0,           0, '',                            0,           1023), -- Milstaff Stormeye
-(     4823,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 25,           0,           0, '',                            0,           1024), -- Milstaff Stormeye
+(     4823,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 24,           0,           0, '',                            0,           1033), -- Milstaff Stormeye
+(     4823,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 25,           0,           0, '',                            0,           1027), -- Milstaff Stormeye
 (     4823,    2,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                406,           0,           0, '',                            0,            462), -- Milstaff Stormeye
 (     4823,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                407,           0,           0, '',                            0,            461), -- Milstaff Stormeye
-(     4825,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 30,           0,           0, '',                            0,           1029), -- Birgitte Cranston
-(     4825,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 31,           0,           0, '',                            0,           1030), -- Birgitte Cranston
+(     4825,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 30,           0,           0, '',                            0,           1036), -- Birgitte Cranston
+(     4825,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 31,           0,           0, '',                            0,           1031), -- Birgitte Cranston
 (     4825,    2,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                422,           0,           0, '',                            0,            462), -- Birgitte Cranston
 (     4825,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                423,           0,           0, '',                            0,            461), -- Birgitte Cranston
-(     4826,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 26,           0,           0, '',                            0,           1025), -- Thuul
-(     4826,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 27,           0,           0, '',                            0,           1026), -- Thuul
+(     4826,    0,             3, 'Please teach me how to teleport to this city.',                          8442,           1,                    1,               -1,               0,                 26,           0,           0, '',                            0,           1037), -- Thuul
+(     4826,    1,             3, 'Please teach me how to create portals to this city.',                       2,           1,                    1,               -1,               0,                 27,           0,           0, '',                            0,           1028), -- Thuul
 (     4826,    3,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                418,           0,           0, '',                            0,            462), -- Thuul
 (     4826,    4,             0, 'Here, I''d like to give you this token of my love.',                    11723,           1,                    1,               -1,               0,                419,           0,           0, '',                            0,            461); -- Thuul
