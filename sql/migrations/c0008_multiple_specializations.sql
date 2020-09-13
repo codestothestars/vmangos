32f554e7524f0c1fe901f86d28d4d9a11839b729
Ariel Oak <codestothestars@yahoo.com>
2020-09-13 14:27:52 -0400

    Enable having both Engineering specializations
    For role-playing. Maintains the mutual exclusivity within the Alliance
    and Horde variants.

--- sql/migrations/c0008_multiple_specializations.sql
+++ sql/migrations/c0008_multiple_specializations.sql
@@ -6,3 +6,7 @@ WHERE `entry` IN (
     5141, 5143, 5144, 5145, 5146, 5148, -- Leatherworking
     5283, 5284, 5301, 5302              -- Blacksmithing
 );
+
+-- Engineering
+UPDATE `quest_template` SET `ExclusiveGroup` = 3630 WHERE `entry` IN (3630, 3632, 3634, 3635, 3637);
+UPDATE `quest_template` SET `ExclusiveGroup` = 3640 WHERE `entry` IN (3640, 3642);
