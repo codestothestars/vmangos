49a72ba46f542d6c807aba8b8b56203da1310251
Ariel Oak <codestothestars@yahoo.com>
2020-09-13 14:21:53 -0400

    Make Aerie Peak NPCs cross-faction compatible
    They are now associated with Ironforge to respond to that faction's
    reputation. Previously we were associating Wildhammer Sentry with
    Stormwind, which is clearly not the right choice.

--- sql/migrations/c0001_cross_faction_questing.sql
+++ sql/migrations/c0001_cross_faction_questing.sql
@@ -5,7 +5,7 @@ UPDATE `creature_loot_template` SET `condition_id` = 0 WHERE `item` IN (
 );
 
 -- Change team-allied NPC factions to city-based versions so players can toggle At War.
-UPDATE `creature_template` SET `Faction` =  11 WHERE `Entry`   IN (7865, 9119); -- Wildhammer Sentry, Muigin from Alliance Generic to Stormwind
+UPDATE `creature_template` SET `Faction` =  11 WHERE `Entry`   = 9119;          -- Muigin from Alliance Generic to Stormwind
 UPDATE `creature_template` SET `Faction` =  11 WHERE `Faction` IN (53, 56);     -- Night Watch to Stormwind
 UPDATE `creature_template` SET `Faction` =  11 WHERE `Faction` IN (150, 1096);                       -- Theramore (hostile to creatures) to Stormwind
 UPDATE `creature_template` SET `Faction` =  12 WHERE `Faction` IN (151, 371, 894, 1075, 1077, 1622); -- Theramore (neutral to creatures) to Stormwind
@@ -14,6 +14,7 @@ UPDATE `creature_template` SET `Faction` =  12 WHERE `Entry` = 1156;            
 UPDATE `creature_template` SET `Faction` =  23 WHERE `Entry` =  417;            -- Felhunter from PLAYER, Gnome to Gnomeregan Exiles
 UPDATE `creature_template` SET `Faction` =  29 WHERE `Entry` IN (10182, 10204); -- Rexxar, Misha from Horde Generic to Orgrimmar
 UPDATE `creature_template` SET `Faction` = 104 WHERE `Entry` = 2987;            -- Eyahn Eagletalon from Horde Generic to Thunder Bluff
+UPDATE `creature_template` SET `Faction` = 122 WHERE `Entry` IN (4782, 5635, 5636, 7744, 7778, 7884, 7865, 8018, 8160, 8161, 9660, 10061, 11097, 11810, 11812, 11813); -- Aerie Peak to Stormwind
 UPDATE `creature_template` SET `Faction` = 123 WHERE `Faction` = 88;            -- Hillsbrad Militia to Stormwind
 UPDATE `creature_template` SET `Faction` = 123 WHERE `Entry` = 14275;           -- Tamra Stormpike to Stormwind
 UPDATE `creature_template` SET `Faction` = 250 WHERE `Entry` = 8284;            -- Dorius Stonetender from Escortee (Alliance) to Escortee (Neutral)
