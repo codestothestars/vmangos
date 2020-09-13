2ca37f2de643e66ae4e06299bb3eb016a1b76d8f
Ariel Oak <codestothestars@yahoo.com>
2020-09-13 14:28:27 -0400

    Enable casting some spells while mounted
    For role-playing.

---
+++ sql/migrations/c11_mounted_spells.sql
@@ -0,0 +1,12 @@
+-- Enables more spells to be cast while mounted
+-- for role-playing and fun.
+
+UPDATE `spell_template` SET `attributes` = `attributes` | 0x01000000 WHERE `entry` IN (
+     883, -- Call Pet
+    6459, -- Ornate Spyglass
+    9885, -- Mark of the Wild (Rank 7)
+    9910, -- Thorns (Rank 6)
+   10714, -- Summon Black Kingsnake
+   21343, -- Snowball
+   23135  -- Heavy Leather Ball
+);
