e91dae3ebd77d1557636d17de4a6c5160870248a
Ariel Oak <codestothestars@yahoo.com>
2020-11-15 14:19:27 -0500

    Sunken Temple, Zul'Farrak, Maraudon reset monthly
    Instead of when the party disbands. Enables extended dungeon sessions.

--- sql/migrations/c0011_longer_instances.sql
+++ sql/migrations/c0011_longer_instances.sql
@@ -2,4 +2,7 @@
 -- to support extended sessions.
 
 UPDATE `creature` SET `spawntimesecsmax` = 2678400, `spawntimesecsmin` = 2678400 WHERE `map` IN (
+    109, -- Sunken Temple
+    209, -- Zul'Farrak
+    349  -- Maraudon
 );
