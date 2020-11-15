f657f828e11b439d71a9b099e028b0c372f00560
Ariel Oak <codestothestars@yahoo.com>
2020-11-15 14:10:19 -0500

    Organize SQL in lower min quest level migration

--- sql/migrations/c0004_lower_minimum_quest_levels.sql
+++ sql/migrations/c0004_lower_minimum_quest_levels.sql
@@ -2,6 +2,9 @@
 -- for role-playing and more adventurous questing.
 
 UPDATE `quest_template` SET `MinLevel` =  0 WHERE `entry` IN (6031, 6032);                               -- Timbermaw Hold
-UPDATE `quest_template` SET `MinLevel` =  0 WHERE `RequiredSkill` = 164;                                 -- Blacksmithing
-UPDATE `quest_template` SET `MinLevel` =  0 WHERE `RequiredSkill` = 202;                                 -- Engineering
 UPDATE `quest_template` SET `MinLevel` = 40 WHERE `entry` IN (7028, 7029, 7041, 7044, 7046, 7064, 7065); -- Maraudon
+
+UPDATE `quest_template` SET `MinLevel` =  0 WHERE `RequiredSkill` IN (
+    164, -- Blacksmithing
+    202  -- Engineering
+);
