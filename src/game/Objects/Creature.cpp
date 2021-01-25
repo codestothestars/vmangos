48529123b175083731ff2e4003f26f6ba88e4338
Ariel Oak <codestothestars@yahoo.com>
2021-01-24 22:24:19 -0500

    Scale Blackrock Depths for 1-DPS groups
    Supports the primary group composition used on our server.

--- src/game/Objects/Creature.cpp
+++ src/game/Objects/Creature.cpp
@@ -1659,6 +1659,7 @@ float Creature::_GetHealthMod(int32 rank)
     uint32 dungeons[] = {
         109, // Sunken Temple
         209, // Zul'Farrak
+        230, // Blackrock Depths
         349  // Maraudon
     };
 
