da5b3d391fce3445c4520289fe181ec77565d0fa
Ariel Oak <codestothestars@yahoo.com>
2021-11-28 18:00:26 -0500

    Remove minimum character level from weapon skills
    For our purposes, this limit on the player is unnecessary.

--- sql/migrations/c0002_skill_min_character_level.sql
+++ sql/migrations/c0002_skill_min_character_level.sql
@@ -11,6 +11,12 @@ WHERE spell IN (
      9786, -- Artisan Blacksmith
     10663, -- Artisan Leatherworking
     10769, -- Artisan Skinning
-    11612, -- Artisan Alchemist,
-    12657  -- Artisan Engineer
+    11612, -- Artisan Alchemist
+    12657, -- Artisan Engineer
+    15984, -- One-Handed Axes
+    15985, -- Two-Handed Axes
+    15986, -- One-Handed Maces
+    15987, -- Two-Handed Maces
+    15992, -- Fist Weapons
+    15996  -- Guns
 );
