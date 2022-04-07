daea0beedbf2b7188b1fe2964fdfecf2b05af5b2
Ariel Oak <codestothestars@yahoo.com>
2022-04-06 22:00:58 -0400

    Update Timbermaw reduced grinding reputation

--- sql/migrations/c10_reduced_grinding.sql
+++ sql/migrations/c10_reduced_grinding.sql
@@ -1,8 +1,8 @@
 UPDATE `creature_loot_template` SET `maxcount` = 2, `mincountOrRef` = 2 WHERE `item` = 8153; -- Wildvine
 
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  80 WHERE `creature_id` IN (7153, 7154, 7155);   -- Deadwood Warrior, Gardener, Pathfinder
-UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  20 WHERE `creature_id` IN (7440, 7441);         -- Winterfall Den Watcher, Totemic
-UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  24 WHERE `creature_id` =  7442;                 -- Winterfall Pathfinder
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  80 WHERE `creature_id` IN (7440, 7441);         -- Winterfall Den Watcher, Totemic
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  96 WHERE `creature_id` =  7442;                 -- Winterfall Pathfinder
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 400 WHERE `creature_id` =  9464 AND `patch` = 0; -- Overlord Ror (Patch <1.9)
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` = 240 WHERE `creature_id` =  9464 AND `patch` = 7; -- Overlord Ror (Patch 1.9+)
 UPDATE `creature_onkill_reputation` SET `RewOnKillRepValue1` =  80 WHERE `creature_id` = 14342 AND `patch` = 0; -- Ragepaw (Patch <1.9)
@@ -12,4 +12,4 @@ UPDATE `quest_template` SET `RewRepValue1` =  100 WHERE `entry` = 6031; -- Runec
 UPDATE `quest_template` SET `RewRepValue1` = 1600 WHERE `entry` = 6131; -- Timbermaw Ally
 UPDATE `quest_template` SET `RewRepValue1` = 1600 WHERE `entry` = 7736; -- Restoring Fiery Flux Supplies via Kingsblood
 UPDATE `quest_template` SET `RewRepValue1` = 3200 WHERE `entry` = 7737; -- Gaining Acceptance
-UPDATE `quest_template` SET `RewRepValue1` =  600 WHERE `entry` = 8464; -- Winterfall Activity
+UPDATE `quest_template` SET `RewRepValue1` = 2400 WHERE `entry` = 8464; -- Winterfall Activity
