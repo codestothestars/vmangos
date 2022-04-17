b822d55b4ebbe57a0303cd1757ea129be49433cb
Ariel Oak <codestothestars@yahoo.com>
2022-04-17 14:10:04 -0400

    Enable casting Mark of the Wild and Thorns mounted

--- sql/migrations/c11_mounted_spells.sql
+++ sql/migrations/c11_mounted_spells.sql
@@ -2,5 +2,7 @@
 -- for role-playing and fun.
 
 UPDATE `spell_template` SET `attributes` = `attributes` | 0x01000000 WHERE `entry` IN (
-    883 -- Call Pet
+     883, -- Call Pet
+    9885, -- Mark of the Wild (Rank 7)
+    9910  -- Thorns (Rank 6)
 );
