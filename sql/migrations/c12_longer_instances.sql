0c4687a1228ca5d1af6ced4507b3d3f238d300d5
Ariel Oak <codestothestars@yahoo.com>
2022-10-03 21:08:43 -0400

    Longer Blackrock Spire reset

--- sql/migrations/c12_longer_instances.sql
+++ sql/migrations/c12_longer_instances.sql
@@ -4,6 +4,7 @@
 UPDATE `creature` SET `spawntimesecsmax` = 2678400, `spawntimesecsmin` = 2678400 WHERE `map` IN (
     109, -- Sunken Temple
     209, -- Zul'Farrak
+    229, -- Blackrock Spire
     230, -- Blackrock Depths
     349, -- Maraudon
     429  -- Dire Maul
