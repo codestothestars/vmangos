f0d9ad4e4ac8685ceddf83412e1c9b6530d6f581
Ariel Oak <codestothestars@yahoo.com>
2023-10-04 22:04:08 -0400

    Lower minimum levels of some quests
    For role-playing and more adventurous questing.

---
+++ sql/migrations/c05_lower_minimum_quest_levels.sql
@@ -0,0 +1,11 @@
+-- Lower minimum levels of some quests
+-- for role-playing and more adventurous questing.
+
+UPDATE `quest_template` SET `MinLevel` =  0 WHERE `entry` IN (6031, 6032);                               -- Timbermaw Hold
+UPDATE `quest_template` SET `MinLevel` = 40 WHERE `entry` IN (7028, 7029, 7041, 7044, 7046, 7064, 7065); -- Maraudon
+
+UPDATE `quest_template` SET `MinLevel` =  0 WHERE `RequiredSkill` IN (
+    164, -- Blacksmithing
+    197, -- Tailoring
+    202  -- Engineering
+);
