3bda2882799d5de6a4d6944d06eeed9ed7ac2a21
Ariel Oak <codestothestars@yahoo.com>
2023-10-04 22:04:08 -0400

    Make leveling pets less grindy

--- src/game/Objects/Pet.cpp
+++ src/game/Objects/Pet.cpp
@@ -1200,6 +1200,8 @@ void Pet::DelayedUnsummon(uint32 timeMSToDespawn, PetSaveMode mode)
 
 void Pet::GivePetXP(uint32 xp)
 {
+    xp *= 3;
+    
     if (getPetType() != HUNTER_PET)
         return;
 
