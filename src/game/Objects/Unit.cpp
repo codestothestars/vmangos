a357cb14a8483396ae92f570e42f8c4bfb84a785
Ariel Oak <codestothestars@yahoo.com>
2025-12-14 14:13:11 -0500

    Enable player heals to stack
    Simplifies multiboxing small parties.

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -3441,7 +3441,11 @@ bool Unit::AddSpellAuraHolder(SpellAuraHolder* holder)
 
             if (stop)
             {
-                RemoveSpellAuraHolder(foundHolder, AURA_REMOVE_BY_STACK);
+                Player* holderPlayer = holder->GetRealCaster()->ToPlayer();
+                Group* group = holderPlayer ? holderPlayer->GetGroup() : nullptr;
+
+                if (!group || !group->IsMember(foundHolder->GetRealCasterGuid()))
+                    RemoveSpellAuraHolder(foundHolder, AURA_REMOVE_BY_STACK);
                 break;
             }
         }
@@ -3690,9 +3694,7 @@ bool Unit::RemoveNoStackAurasDueToAuraHolder(SpellAuraHolder* holder)
         if (i_spellId == spellId)
         {
             // Nostalrius - fix stack same HoT rank / diff caster
-            if (firstInChain)
-                aurasToRemove.emplace_back(spellId, i.second->GetCasterGuid());
-            else switch (spellId)
+            switch (spellId)
             {
             // Blessing of Light does not stack between casters.
                 case 19977:
