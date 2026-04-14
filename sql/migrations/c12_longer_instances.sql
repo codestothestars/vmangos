495157706a21c8a93353ede37571db0a4e72e939
Ariel Oak <codestothestars@yahoo.com>
2026-04-13 20:42:07 -0400

    Support dungeons expiring only after 1 month
    Instead of when the party disbands. Enables extended dungeon sessions.

---
+++ sql/migrations/c12_longer_instances.sql
@@ -0,0 +1,15 @@
+-- Makes instances reset only after a month
+-- to support extended sessions.
+
+UPDATE `creature` SET `spawntimesecsmax` = 2678400, `spawntimesecsmin` = 2678400 WHERE `map` IN (
+    109, -- Sunken Temple
+    209, -- Zul'Farrak
+    229, -- Blackrock Spire
+    230, -- Blackrock Depths
+    249, -- Onyxia's Lair
+    289, -- Scholomance
+    329, -- Stratholme
+    349, -- Maraudon
+    409, -- Molten Core
+    429  -- Dire Maul
+);
